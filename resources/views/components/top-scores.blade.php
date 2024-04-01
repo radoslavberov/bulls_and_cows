a53e1dce023932cb8c65a171dd6a193463bc79a8
Radoslav Berov <radoslavv.berov@gmail.com>
2024-04-01 19:22:31 +0300

    add style changes

--- resources/views/components/top-scores.blade.php
+++ resources/views/components/top-scores.blade.php
@@ -1,6 +1,6 @@
 <div class="card">
     <div class="card-body">
-        <h5 class="card-title">Top Scores</h5>
+        <div class="card-title custom-header text-center">Top Scores</div>
         @if(count($topScores) > 0)
             @foreach($topScores as $score)
                 <div class="alert alert-primary">User: {{ $score['user']['email']}},
--- resources/views/game.blade.php
+++ resources/views/game.blade.php
@@ -7,28 +7,32 @@
             <div class="col-md-8">
                 <div class="card custom-card">
                     <div class="card-body">
-                        <h3 class="card-title text-center">Guess the 4-digit number!</h3>
-                        <h5>Attempts: <span id="attempts-count">{{ session('attempts', 0) }}</span></h5>
+                        <div class="card-title custom-header text-center">Guess the 4-digit number!</div>
+                        <h6>Attempts: <span id="attempts-count">{{ session('attempts', 0) }}</span></h6>
                         <form action="{{ route('game.guess') }}" method="POST" id="guess-form">
                             @csrf
-                            <div class="form-group">
+                            <div class="form-group custom-form-group">
                                 <label for="guess">Enter your guess:</label>
-                                <input type="text" class="form-control @error('guess') is-invalid @enderror" id="guess" name="guess" pattern="\d{4}"
-                                       maxlength="4" required>
+                                <input type="text" class="form-control @error('guess') is-invalid @enderror" id="guess"
+                                       name="guess" pattern="\d{4}" maxlength="4" required>
                                 @error('guess')
                                 <div class="alert alert-danger">{{ $message }}</div>
                                 @enderror
                             </div>
-                            <button type="submit" class="btn btn-primary btn-md btn-block">Submit Guess</button>
+                            <button type="submit" class="btn btn-primary btn-md btn-block custom-button">Submit Guess
+                            </button>
                         </form>
                         <form action="{{ route('game.finish') }}" method="POST" id="give-up-form" class="mt-3">
                             @csrf
-                            <button type="submit" class="btn btn-warning btn-md btn-block">Give Up</button>
+                            <button type="submit" class="btn btn-warning btn-md btn-block custom-button">Give Up
+                            </button>
                         </form>
 
                         <form action="{{ route('game.start-new') }}" method="POST">
                             @csrf
-                            <button type="submit" class="btn btn-success btn-md btn-block mt-3">Start New Game</button>
+                            <button type="submit" class="btn btn-success btn-md btn-block custom-button">Start New
+                                Game
+                            </button>
                         </form>
                         <div class="results-container mt-3">
                             <div id="guess-results"></div>
@@ -37,14 +41,15 @@
                 </div>
             </div>
             <div class="col-md-4">
-                <x-top-scores />
+                <x-top-scores/>
                 <!-- User scores display -->
                 <div class="card mt-3 users-scores">
                     <div class="card-body">
-                        <h5 class="card-title">Your Scores</h5>
+                        <div class="card-title custom-header text-center">Your Scores</div>
                         @if(count($userScores) > 0)
                             @foreach($userScores as $score)
-                                <div class="alert alert-primary">Game ID: {{ $score['id'] }}, Attempts: {{ $score['attempts'] }}</div>
+                                <div class="alert alert-primary">Game ID: {{ $score['id'] }},
+                                    Attempts: {{ $score['attempts'] }}</div>
                             @endforeach
                         @else
                             <div class="alert alert-info">You haven't completed any games yet.</div>
