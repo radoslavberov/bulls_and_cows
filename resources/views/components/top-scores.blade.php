582a13b168db84887df9c07eef055b1bb7d4ca44
Radoslav Berov <radoslavv.berov@gmail.com>
2024-04-01 14:14:20 +0300

    show/hide top scores

--- resources/views/components/top-scores.blade.php
+++ resources/views/components/top-scores.blade.php
@@ -1,9 +1,13 @@
 <div class="card">
     <div class="card-body">
         <h5 class="card-title">Top Scores</h5>
-        @foreach($topScores as $score)
-            <div class="alert alert-primary">User: {{ $score['user']['email']}},
-                Best Attempt: {{ $score['best_attempt'] }}</div>
-        @endforeach
+        @if(count($topScores) > 0)
+            @foreach($topScores as $score)
+                <div class="alert alert-primary">User: {{ $score['user']['email']}},
+                    Best Attempt: {{ $score['best_attempt'] }}</div>
+            @endforeach
+        @else
+            <div class="alert alert-info">No Top Scores Yet! Be The First At The Top!</div>
+        @endif
     </div>
 </div>
